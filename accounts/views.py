9e7ca31247e37eaeb5a4ce164adfabb1e703c8d3
Nayli Nabila <73385572+NayliNabila@users.noreply.github.com>
2021-02-27 03:41:59 +0800

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -6,15 +6,15 @@ from django.contrib.auth.forms import UserCreationForm
 #def home (request):
 #    return render (request, 'home.html')
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+def signup(request):                                #to allow user for sign up
+    if request.method == 'POST':                  
+        form = UserCreationForm(request.POST)       
+        if form.is_valid():                         
+            form.save()                             # data of user will be save
             return render (request, 'home.html')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {
+    return render(request, 'signup.html', {         # to display the form of sign up
         'form' : form
     })
 
