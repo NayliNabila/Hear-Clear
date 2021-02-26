23508e1a7d819d52ec6ebb5b73c26455899e9c85
Nayli Nabila <73385572+NayliNabila@users.noreply.github.com>
2021-02-27 03:45:19 +0800

    Update models.py

--- page/models.py
+++ page/models.py
@@ -4,18 +4,18 @@ from django.utils import timezone
 from django.urls import reverse
 from django.core.files.images import ImageFile
 
-import librosa 
+import librosa                                  #importing librosa to analyze the song
 import librosa.display
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt                 # matplotlib will be use for the plotting
 import numpy as np
 import sys
 import os
 
 # Create your models here.
 
-class Profile(models.Model):
+class Profile(models.Model):                                                # class for creating table for editing the profile
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
 
@@ -24,7 +24,7 @@ class Profile(models.Model):
 
 
 
-class SongFile(models.Model):
+class SongFile(models.Model):                                               # class for accepting and analyzing the audio 
     title = models.CharField(max_length = 100, unique=True)
     audio = models.FileField(upload_to= 'songs')
     image = models.ImageField(upload_to='audioimage/image')
@@ -41,7 +41,7 @@ class SongFile(models.Model):
     def get_absolute_url(self):
         return reverse('songfile-detail', kwargs={'pk': self.pk})
 
-    def save (self, *args, **kwargs):
+    def save (self, *args, **kwargs):                                       # process of analyzing audio start 
         super(SongFile,self).save(*args, **kwargs)
         dir_image = "./media/audioimage/image"
         imagePath = dir_image + str(timezone.now()) + ".png"
@@ -60,13 +60,13 @@ class SongFile(models.Model):
         self.samp_freq = sr
         super(SongFile,self).save(*args, **kwargs)
 
-        def delete(self, *args, **kwargs):
+        def delete(self, *args, **kwargs):                                  # function for deleting the song 
             self.title.delete()
             self.audio.delete()
             self.image.delete()
             super().delete(*args, **kwargs)
 
-class Comments(models.Model):
+class Comments(models.Model):                                               # class for commenting part 
     songfile = models.ForeignKey(SongFile, related_name="comments", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     body = models.TextField()
